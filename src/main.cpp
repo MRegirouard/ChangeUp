b72dc9bab0bbb969d5de8cd7ffa4646fd3b2b694
Ethan Girouard <mr.egirouard@gmail.com>
2020-06-10 11:12:54 -0400

    Clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Thu Sep 26 2019                                           */
-/*    Description:  Competition Template                                      */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
@@ -22,11 +13,7 @@
 #include "Robot.h"
 
 using namespace vex;
-
-// A global instance of competition
 competition Competition;
-
-// define your global instances of motors and other devices here
 
 float WheelDiameter = 3.25; // (inches)
 float Width = 19; // Middle of wheel to middle of opposite wheel (inches)
@@ -36,41 +23,14 @@ int ForwardSpeed;
 int RightSpeed;
 int RotationSpeed;
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
-
-void pre_auton(void) {
+void pre_auton(void)
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-
-
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-
+void autonomous(void)
+{
   printf("Running autonomous...\n");
   //Robot.Move(0, 0, 90, 50, true);
   Robot.Move(0, 24, 0, 50);
@@ -78,30 +38,12 @@ void autonomous(void) {
   printf("Autonomous complete.\n");
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void usercontrol(void) {
-  // User control code here, inside the loop
+void usercontrol(void)
+{
   printf("Running driver control...\n");
 
-  while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
-
+  while (true)
+  {
     ForwardSpeed = Controller1.Axis2.position();
     RightSpeed = Controller1.Axis1.position();
     RotationSpeed = Controller1.Axis4.position();
@@ -112,21 +54,19 @@ void usercontrol(void) {
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
-int main() {
+int main()
+{
   printf("Beginning main program...\n");
 
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
-  // Run the pre-autonomous function.
-  pre_auton();
+  pre_auton(); // Run the pre-autonomous function.
 
   // Prevent main from exiting with an infinite loop.
-  while (true) {
+  while (true)
+  {
     wait(100, msec);
   }
 }
