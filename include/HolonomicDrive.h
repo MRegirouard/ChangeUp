71b976437544b4c51f1bf5481dbd4f3af22b6c71
Ethan Girouard <mr.egirouard@gmail.com>
2020-09-26 18:55:37 -0400

    Add position calculation

--- include/HolonomicDrive.h
+++ include/HolonomicDrive.h
@@ -2,17 +2,24 @@
 #define HOLONOMICDRIVECLASS_H
 
 #include "Wheel.h"
+#include "AngleConversions.h"
 
 class HolonomicDrive
 {
   public:
-    HolonomicDrive(Wheel *Wheels, int WheelCount, float WheelCircumferece);
+    HolonomicDrive(Wheel *Wheels, int WheelCount, float DriveDiameter);
     
     Wheel *Wheels;
     int WheelCount;
 
     void MoveTo(int ForwardDistance, int RightDistance, int RoationDegrees);
     void SetSpeed(int ForwardSpeed, int RightSpeed, int RotationSpeed);
+    void UpdatePosition();
+    void ResetPosition();
+    double Forward, Right, Rotation;
+
+  private:
+    float Circumference;
 };
 
 #endif // HOLONOMICDRIVECLASS_H
--- src/HolonomicDrive.cpp
+++ src/HolonomicDrive.cpp
@@ -1,9 +1,9 @@
 #include "HolonomicDrive.h"
-#include "AngleConversions.h"
 
-HolonomicDrive::HolonomicDrive(Wheel *Wheels, int WheelCount, float WheelCircumference)
+HolonomicDrive::HolonomicDrive(Wheel *Wheels, int WheelCount, float DriveDiameter)
 {
   this->WheelCount = WheelCount;
+  Circumference = M_PI * DriveDiameter;
 }
 
 void HolonomicDrive::MoveTo(int ForwardDistance, int RightDistance, int RoationDegrees)
@@ -21,4 +21,32 @@ void HolonomicDrive::SetSpeed(int ForwardSpeed, int RightSpeed, int RotationSpee
     Wheels[W].setVelocity(Magnitude * sin((Angle - Wheels[W].Angle + 90) * toRad) + RotationSpeed, percent);
     Wheels[W].spin(forward);
   }
+}
+
+void HolonomicDrive::UpdatePosition()
+{
+  float ForwardChange = 0;
+  float RightChange = 0;
+  float RotationChange = 0;
+
+  for (int W = 0; W < WheelCount; W++)
+  {
+    ForwardChange += sin((Rotation - Wheels[W].Angle + 90) * toRad) * Wheels[W].position(rev) * Wheels[W].Circumference;
+    RightChange += sin((Rotation - Wheels[W].Angle + 90) * toDeg) * Wheels[W].position(rev) * Wheels[W].Circumference;
+    RotationChange += Wheels[W].position(rev) * Wheels[W].Circumference / Circumference;
+    Wheels[W].resetPosition();
+  }
+
+  Forward += ForwardChange;
+  Right += RightChange;
+  Rotation += RotationChange;
+
+  printf("Robot Positio: (%f in, %f in, %f°)", Forward, Right, Rotation);
+}
+
+void HolonomicDrive::ResetPosition()
+{
+  Forward = 0;
+  Right = 0;
+  Rotation = 0;
 }
