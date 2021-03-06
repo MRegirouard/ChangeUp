f3502647ea006909dd36fc688d22fb58eb81d0d2
Ethan Girouard <mr.egirouard@gmail.com>
2021-03-05 19:03:08 -0500

    Add robot devices/ports

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// FrontLeft            motor         11              
+// FrontRight           motor         19              
+// BackLeft             motor         12              
+// BackRight            motor         18              
+// LeftIntake           motor         1               
+// RightIntake          motor         17              
+// LeftLift             motor         2               
+// RightLift            motor         16              
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -8,8 +19,7 @@ competition Competition;
 
 void pre_auton()
 {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
+  
 }
 
 void autonomous()
@@ -30,6 +40,9 @@ int main()
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
+
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
   
   pre_auton(); // Run the pre-autonomous function.
 
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,15 +1,32 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor FrontLeft = motor(PORT11, ratio18_1, false);
+motor FrontRight = motor(PORT19, ratio18_1, true);
+motor BackLeft = motor(PORT12, ratio18_1, false);
+motor BackRight = motor(PORT18, ratio18_1, true);
+motor LeftIntake = motor(PORT1, ratio18_1, true);
+motor RightIntake = motor(PORT17, ratio18_1, false);
+motor LeftLift = motor(PORT2, ratio18_1, true);
+motor RightLift = motor(PORT16, ratio18_1, false);
+controller Controller1 = controller(primary);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
 
 /**
- * Used to initialize code/tasks/devices added using tools in VEXcode Text.
- *
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  // nothing to initialize
 }
